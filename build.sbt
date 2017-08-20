74b98cd1f08f2babcff42c74c153860146d1ae86
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2017-08-20 18:17:35 +0900

    Init project

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "finatra-toy"
+
+version := "1.0"
+
+scalaVersion := "2.12.3"
+
+libraryDependencies ++= Seq(
+  "com.twitter" %% "finatra-http" % "2.12.0"
+)
---
+++ src/main/scala/yokohama/murataku/ft/MyServer.scala
@@ -0,0 +1,9 @@
+package yokohama.murataku.ft
+
+class MyServer extends HttpServer {
+
+  /*
+  override def configureHttp(router : HttpRouter): Unit = {
+  }
+  */
+}
